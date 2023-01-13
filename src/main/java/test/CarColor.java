4dd5dd1eb968296b93d05171efee8422c1c80a38
Bastien Robert <bastien.robert.01@efrei.net>
2023-01-13 20:13:58 +0100

    Revert "Revert "End project, delete files from TP correction and added new resequencer + filter to complicate the project and try to get a better grade""
    This reverts commit b2445420f1b44fcd064f0b3f05a867f7ff7d5288.

--- src/main/java/test/CarColor.java
+++ src/main/java/test/CarColor.java
@@ -3,7 +3,7 @@ package test;
 public class CarColor {
     public MyCar ChangingColor(MyCar myCar){
         if (myCar.getName().equals("Tesla model X Plaid")) {
-            myCar.setColor("black");
+            myCar.setColor("grey");
         }
         return myCar;
     }
--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -9,42 +9,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-		//ApplicationContext ctx = new ClassPathXmlApplicationContext("serviceActivator.xml");
-
-		//ApplicationContext ctx = new ClassPathXmlApplicationContext("routing.xml");
-
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("CarProject.xml");
-
-		//ApplicationContext ctx = new ClassPathXmlApplicationContext("filtering.xml");
-
-		/*ApplicationContext ctx = new ClassPathXmlApplicationContext("javaScriptService.xml");
-
-		MyGateway myGateway= (MyGateway)ctx.getBean("gateWayService");
-		myGateway.method("Salut !");*/
-
-
-
-		/*ApplicationContext ctx = new ClassPathXmlApplicationContext("webServiceCalling.xml");
-
-		MyGateway myGateway= (MyGateway)ctx.getBean("gateWayService");
-		myGateway.method("Salut !");*/
-
-
-
-		/*ApplicationContext ctx = new ClassPathXmlApplicationContext("database.xml");
-
-		PersonService personService = ctx.getBean(PersonService.class);
-		
-		Person person = new Person();
-		person.setName("Tintin");
-		person = personService.createPerson(person);
-
-		System.out.println(person);*/
-
 
 	}
 
 }
- 
